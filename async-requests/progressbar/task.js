7ef83378a52027531fb039e02e51f85cd5d8c45d
Vlad <laushvl@gmail.com>
2025-05-12 17:34:38 +0400

    Fix progressbar

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -7,7 +7,7 @@ form.addEventListener('submit', (e) => {
     const formData = new FormData(form);
     const xhr = new XMLHttpRequest();
     xhr.upload.addEventListener('progress', e => {
-        progress.value = e.loaded;
+        progress.value = e.loaded / e.total;
     })
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
     xhr.send(formData);
